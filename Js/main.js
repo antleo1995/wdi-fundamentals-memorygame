092c88f8c3c4650b3560afc30e7a19d1f541bbd1
Tony L <tonyl@Tonys-MacBook-Pro.local>
2017-03-09 21:01:11 -0800

    Added game additional game logic

--- Js/main.js
+++ Js/main.js
@@ -1,8 +1,24 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
-console.log("User flipped" + " " + cardOne); 
-console.log("User flipped" + " " + cardThree); 
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[1];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped" + " " + cardsInPlay[0]); 
+console.log("User flipped" + " " + cardsInPlay[1]); 
+console.log(cardsInPlay.length);
+
+if (cardsInPlay.length === 2) {
+	console.log("getting there");
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert ("You found a match!");
+	} else {
+		alert ("Sorry, try again.")
+	}
+}
+
+
 //comment to save
