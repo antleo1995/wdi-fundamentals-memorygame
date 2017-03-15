c5b7fb1b78e5e3b60398067ca037dcfb6f423584
antleo1995 <antleo1995@gmail.com>
2017-03-15 14:15:15 -0700

    Final changes

--- Js/main.js
+++ Js/main.js
@@ -24,10 +24,10 @@ var cards = [
 ];
 var cardsInPlay = [];
 
-//first checks array length and if long enough executes code to compare cards
+//function - first checks array length and if long enough executes code to compare cards
 var checkForMatch= function (){
 if (cardsInPlay.length === 2 ) {	
-		//console.log("enough cards");
+		//console.log("enough cards"); - code check for if things were working
 		if (cardsInPlay[0] === cardsInPlay[1]) {
 			alert ("You found a match!");
 		} else {
@@ -38,14 +38,27 @@ if (cardsInPlay.length === 2 ) {
 	    console.log("Keep flipping!")
 	}
 }
-//Writes to console card flipped and runs checkForMatch
-var flipCard = function (cardId){
-	console.log ("User flipped " + cards[cardId].rank);
-	cardsInPlay.push(cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
-	checkForMatch();
+//function - get data id of card, adds to cards played and checks for match
+var flipCard = function (){
+		cardId = this.getAttribute('data-id');
+		console.log(cardId);
+		this.setAttribute("src", cards[cardId].cardImage);
+		console.log ("User flipped " + cards[cardId].rank + " of " + cards[cardId].suit);
+		cardsInPlay.push(cards[cardId].rank);
+		//run check
+		checkForMatch();
 	}
-//mimmicking user input
-flipCard(0);
-flipCard(2);
+//create the board of cards
+var createBoard = function (){
+	for (i = 0; i < cards.length; i++)
+	{
+		var cardElement = document.createElement("img");
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener("click", flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+		console.log("Adding Card " + i);
+	}
+}
+//creates the cards
+createBoard();
